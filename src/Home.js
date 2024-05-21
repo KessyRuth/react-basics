6824a51008682262d39c50cdd6a25955dde42c5b
Kessy <akinkessy3@gmail.com>
2024-05-21 18:42:01 +0300

    Video 12,13,14,15,16

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,31 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import Bloglist from './Bloglist';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
       {title:'My new website', body:'lorem ipsum...', author: 'Kessy', id:1},
       {title:'Welcome party!', body:'lorem ipsum...', author: 'Leo', id:2},
       {title:'With dev top tips', body:'lorem ipsum...', author: 'Trevor', id:3}
-    ])
+    ]);
+
+    const [name, setName]= useState('Kessy');
+
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !==id);
+      setBlogs(newBlogs);
+    }
+
+    useEffect(()=> {
+      console.log("Use effect run")
+    }, [])
    return (  
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                  <h2>{blog.title}</h2>
-                  <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <Bloglist blogs={blogs} title='All blogs' handleDelete={handleDelete}/>
+            <Bloglist blogs={blogs.filter((blog)=>blog.author==='Kessy')} title="Kessy's blog"/>
+            <button onClick={()=>setName('Trevor')}>Change name</button>
+            <p>{name}</p>
             </div>
     );
-}
  
+}
 export default Home;
